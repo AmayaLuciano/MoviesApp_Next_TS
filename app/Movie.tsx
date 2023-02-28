ac9549ab45018eedd162d18d0d893783aefb60ec
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2023-02-28 18:32:57 -0300

    fix error

--- app/Movie.tsx
+++ app/Movie.tsx
@@ -2,21 +2,21 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { FC } from 'react';
 
-// export interface Props {
-//   id: number;
-// }
+export interface Props {
+  id: number;
+}
 
-const Movie: FC = () => {
+const Movie: FC<Props> = () => {
   const imagePath = 'https://image.tmdb.org/t/p/original';
   return (
     <div className="flex flex-col justify-end text-center">
-      <div>
-        {/* <div className="mb-3">
+      {/* <Link href={`/${id}`}>
+        <div className="mb-3">
           <h1>{title}</h1>
           <h3>{releaseDate}</h3>
         </div>
-        <Image src={imagePath + image} width={600} height={600} alt={title} /> */}
-      </div>
+        <Image src={imagePath + image} width={600} height={600} alt={title} />
+      </Link> */}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { getMovies } from './api/movies';
-import Movie from './Movie';
 import { Movies } from './Types/types';
 
 export default async function Home() {
   const data: Movies = await getMovies();
   const movies = data.results;
+
+  const imagePath = 'https://image.tmdb.org/t/p/original';
   return (
     <main>
       <div className="grid gap-16 grid-cols-fluid">
         {movies.map((movie) => (
-          <p key={movie.id}>{movie.title}</p>
+          <div key={movie.id} className="flex flex-col justify-end text-center">
+            <Link href={`/${movie.id}`}>
+              <div className="mb-3">
+                <h1>{movie.title}</h1>
+                <h3>{movie.release_date}</h3>
+              </div>
+              <Image
+                src={imagePath + movie.poster_path}
+                width={600}
+                height={600}
+                alt={movie.title}
+              />
+            </Link>
+          </div>
         ))}
       </div>
     </main>
