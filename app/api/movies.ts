705e4cfb91e12e92509e3330fba79ae5ece5a2c8
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2023-03-01 14:34:54 -0300

    fix error

--- app/api/movies.ts
+++ app/api/movies.ts
@@ -2,7 +2,7 @@ import { Movies } from "../Types/types";
 
 export const getMovies = async () : Promise<Movies>  => {
     const data = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=0323e40e53318e3d94d399ea4461b83e`
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
       );
     
       const res = await data.json();
