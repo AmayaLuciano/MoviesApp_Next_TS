8afde499a3219f988f419a836b66aa753d19476b
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2023-03-01 14:27:48 -0300

    fix error

--- app/[movie]/page.tsx
+++ app/[movie]/page.tsx
@@ -11,7 +11,7 @@ const MovieDetail = async ({ params }: Props) => {
   const imagePath = 'https://image.tmdb.org/t/p/original';
 
   const data = await fetch(
-    `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`
+    `https://api.themoviedb.org/3/movie/${movie}?api_key=0323e40e53318e3d94d399ea4461b83e`
   );
 
   const res: MovieDetail = await data.json();
